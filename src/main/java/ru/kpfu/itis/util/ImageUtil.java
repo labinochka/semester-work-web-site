f0672b0838bb23536654b6daac2c790b49ea98fb
labinochka <labensckayam@yandex.ru>
2023-10-19 22:22:03 +0300

    update

--- src/main/java/ru/kpfu/itis/util/ImageUtil.java
+++ src/main/java/ru/kpfu/itis/util/ImageUtil.java
@@ -1,15 +1,10 @@
 package ru.kpfu.itis.util;
 
-import ru.kpfu.itis.service.AccountService;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
 import java.io.*;
-import java.nio.file.Paths;
 
 public class ImageUtil {
-
-    static AccountService accountService = new AccountService();
 
     public static String makeFile(Part part, String fileName, HttpServletRequest req) {
         try {
