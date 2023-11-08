05e4707547e03cbca7ba35f3aae1778325ff27d5
labinochka <labensckayam@yandex.ru>
2023-11-08 20:49:03 +0300

    add properties

--- src/main/java/ru/kpfu/itis/util/ConnectionProvider.java
+++ src/main/java/ru/kpfu/itis/util/ConnectionProvider.java
@@ -1,11 +1,27 @@
 package ru.kpfu.itis.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionProvider {
     private static ConnectionProvider _instance;
+
+    Properties property = new Properties();
+
+    {
+        try {
+            ClassLoader classloader = getClass().getClassLoader();
+            InputStream is = classloader.getResourceAsStream("application.properties");
+            property.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static ConnectionProvider getInstance() throws DbException {
         if (_instance == null) {
@@ -14,10 +30,10 @@ public class ConnectionProvider {
         return _instance;
     }
 
-    final String DRIVER = "org.postgresql.Driver";
-    final String HOST = "jdbc:postgresql://localhost:5432/beer_site";
-    final String USER = "postgres";
-    final String PASS = "password";
+    final String DRIVER = property.getProperty("db.driver.classname");
+    final String HOST = property.getProperty("db.url");;
+    final String USER = property.getProperty("db.username");;
+    final String PASS = property.getProperty("db.password");;
 
     private Connection connection;
 
